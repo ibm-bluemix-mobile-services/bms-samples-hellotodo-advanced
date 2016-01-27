028fdd27e3dfc217911a2e830c06ac310bb43b66
David Cariello <drcariel@us.ibm.com>
2016-01-27 17:06:36 -0500

    Remove duplicate dependency on core framework, set build directory to reduce path length, and fixed bug with facebook login

--- Android/helloTodoAdvanced/app/src/main/java/com/ibm/hellotodoadvanced/MainActivity.java
+++ Android/helloTodoAdvanced/app/src/main/java/com/ibm/hellotodoadvanced/MainActivity.java
@@ -122,9 +122,9 @@ public class MainActivity extends Activity implements ResponseListener {
      */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
         FacebookAuthenticationManager.getInstance().onActivityResultCalled(requestCode, resultCode, data);
-
     }
 
     /**
